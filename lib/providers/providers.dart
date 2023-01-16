95a4b321b134a6f05513e540d452428e0d886161
sahilsingh <sahil@emotionallyspeaking.com>
2023-01-16 13:42:06 +0530

    Added seetings for iOS and deleted unnecessary files

--- lib/providers/providers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:provider/provider.dart';
-
-import 'favorites_provider.dart';
-import 'upcoming_launches_provider.dart';
-
-List<ChangeNotifierProvider> providers = [
-  ChangeNotifierProvider(create: (_) => LaunchesProvider()),
-  ChangeNotifierProvider(create: (_) => FavoritesProvider()),
-];
